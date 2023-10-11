a9fd8a447ffeecb0df1c64d9c3906502c207dfde
Madsam999 <valet-moire0s@icloud.com>
2023-10-11 00:58:55 -0400

    Added the second case. A refinement of the first one now with 2 cards

--- dragAndDropTest/dragAndDropTest.js
+++ dragAndDropTest/dragAndDropTest.js
@@ -30,8 +30,16 @@ class Card {
     }
 }
 
+var col3 = []
+var col4 = []
+
 var card1 = new Card(10,"club")
 var card2 = new Card(9,"diamond")
+
+var cardInMovement = -1
+
+col3.push(card1)
+col4.push(card2)
 
 function allowDrop2(ev) {
 	ev.preventDefault();
@@ -40,10 +48,54 @@ function allowDrop2(ev) {
 function drag2(ev) {
 	ev.dataTransfer.setData("text", ev.target.id);
     console.log(ev.target);
+    checkCard(ev);
 }
 
-function drop2(ev) {
+function drop2(ev,position) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    if(checkIfCanMove(position)) {
+        if(ev.target.tagName.toLowerCase() == "img") {
+            var data = ev.dataTransfer.getData("text")
+            ev.target.parentElement.appendChild(document.getElementById(data));
+        }
+        else {
+            var data = ev.dataTransfer.getData("text")
+            ev.target.appendChild(document.getElementById(data));
+        }
+    }
+    else {
+        console.log("nuhuh")
+    }
 }
+
+function checkCard(event) {
+    if(event.target.id == "image2") {
+        cardInMovement = col3[0]
+    }
+    else {
+        cardInMovement = col4[0]
+    }
+}
+
+function checkIfCanMove(position) {
+    switch(position) {
+        case 3:
+            if(cardInMovement.value < col3[0].value && col3[0].colour != cardInMovement.colour) {
+                return true
+            }
+            else {
+                return false
+            }
+        case 4:
+            if(cardInMovement.value < col4[0].value && col4[0].colour != cardInMovement.colour) {
+                return true
+            }
+            else {
+                return false
+            }
+    }
+}
+
+// -----------------------------------------------------------
+

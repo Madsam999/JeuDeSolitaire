1fa69fd8169cea1c9615dff769fccc9953002a9c
Madsam999 <valet-moire0s@icloud.com>
2023-10-10 23:17:44 -0400

    First drag and drop test coded

--- dragAndDropTest/dragAndDropTest.js
+++ dragAndDropTest/dragAndDropTest.js
@@ -1,15 +1,48 @@
 // This is a super simple case of the drag and drop model. I move the card from div 1 to div 2
 
-function allowDrop1(ev) {
+function allowDrop(ev) {
 	ev.preventDefault();
 }
 
-function drag1(ev) {
+function drag(ev) {
 	ev.dataTransfer.setData("text", ev.target.id);
     console.log(ev.target);
 }
 
-function drop1(ev) {
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+// ---------------------------------------------------------------
+
+class Card {
+    constructor(value,suite) {
+        this.value = value
+        this.suite = suite
+        if(this.suite == "club" || this.suite == "ace" ) {
+            this.colour = 0
+        }
+        else {
+            this.colour = 1
+        }
+    }
+}
+
+var card1 = new Card(10,"club")
+var card2 = new Card(9,"diamond")
+
+function allowDrop2(ev) {
+	ev.preventDefault();
+}
+
+function drag2(ev) {
+	ev.dataTransfer.setData("text", ev.target.id);
+    console.log(ev.target);
+}
+
+function drop2(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
